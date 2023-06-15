c5ecb295b84b10f9f3c62a674c78d270a1e71e77
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2023-06-15 10:32:19 -0500

    feat: add .gitignore

---
+++ RecipeSearching/RecipeSearching.h
@@ -0,0 +1,18 @@
+//
+//  RecipeSearching.h
+//  RecipeSearching
+//
+//  Created by Luu Van on 6/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeSearching.
+FOUNDATION_EXPORT double RecipeSearchingVersionNumber;
+
+//! Project version string for RecipeSearching.
+FOUNDATION_EXPORT const unsigned char RecipeSearchingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeSearching/PublicHeader.h>
+
+
---
+++ SharedNetwork/SharedNetwork.h
@@ -0,0 +1,18 @@
+//
+//  SharedNetwork.h
+//  SharedNetwork
+//
+//  Created by Luu Van on 6/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SharedNetwork.
+FOUNDATION_EXPORT double SharedNetworkVersionNumber;
+
+//! Project version string for SharedNetwork.
+FOUNDATION_EXPORT const unsigned char SharedNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedNetwork/PublicHeader.h>
+
+
