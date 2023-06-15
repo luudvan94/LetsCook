c82ca2b75a7a36ec953187ea6b9ea852271e6a10
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2023-06-15 09:51:41 -0500

    Initial Commit

---
+++ LetsCook/LetsCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsCookApp.swift
+//  LetsCook
+//
+//  Created by Luu Van on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LetsCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
