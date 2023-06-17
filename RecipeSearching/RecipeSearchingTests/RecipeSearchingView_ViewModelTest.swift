6122d0fa735ff1c47ace183315f80d3816904b4c
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2023-06-16 22:41:29 -0500

    feat(recipe-searching): add empty test file

---
+++ RecipeSearching/RecipeSearchingTests/RecipeSearchingView_ViewModelTest.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeSearchingView_ViewModelTest.swift
+//  RecipeSearchingTests
+//
+//  Created by Luu Van on 6/16/23.
+//
+
+import XCTest
+
+final class RecipeSearchingView_ViewModelTest: XCTestCase {
+
+}
